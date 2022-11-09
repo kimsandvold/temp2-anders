1695149ea256e1a066dbb560323f6a3af2c44b57
gjermundkvardal <gjermund@rubic.no>
2022-11-09 13:22:51 +0100

    Completed styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './rubichvit.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Header'>
+        <a href="https://www.rubic.no/"><img src={logo} className="logo" /> </a>
+        <h2 className='topTekst'> Vinlotteri for Rubic AS </h2>
+      </div>
+     
     </div>
   );
 }
