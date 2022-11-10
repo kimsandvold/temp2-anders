420f1ccce5dc171a8ebafbc8124089a6814cd7b1
Andrrs64 <andersthorsrud@hotmail.no>
2022-11-10 10:53:25 +0100

    Fixed min, max on button

--- src/components/Button.component.js
+++ src/components/Button.component.js
@@ -5,7 +5,7 @@ function Button(){
     const max = 30
 
     function trekk(){
-        const rand = Math.floor(Math.random() * max - min + 1) + min
+        const rand = Math.floor(Math.random() * (max + 1) - min) + min
     
         console.log(rand)
     }
