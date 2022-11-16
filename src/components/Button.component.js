0175e83020657d493092dec4f36d0a8ed07ffc6a
Andrrs64 <andersthorsrud@hotmail.no>
2022-11-16 09:43:20 +0100

    added error message

--- src/components/Button.component.js
+++ src/components/Button.component.js
@@ -1,17 +1,19 @@
 import {useEffect, useState} from "react"
 
 function Button(props){
-    const {amountOfParticipants, numHandler, setDrawState} = props
+    const {amountOfParticipants, numHandler, setDrawState, displayError} = props
 
     const max = amountOfParticipants
 
     const [trekkNum,setTrekkNum] = useState();
     const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('')
 
     const trekk = (i) => {
         if (amountOfParticipants > 1){
             setDrawState('Trekker')
             setButtonDisabled(true)
+            setErrorMessage('')
             if (i > 0) {
                 setTrekkNum(i - (Math.floor(i / amountOfParticipants) * amountOfParticipants))
                 setTimeout(trekk, 150 - i, i - 1)
@@ -21,12 +23,15 @@ function Button(props){
                 setDrawState('Vinner')
                 setButtonDisabled(false)
             }
+        }else {
+            setErrorMessage('Du må velge minst 2 navn for å trekke')
         }
     }
 
     useEffect(() => {
             numHandler(trekkNum)
-    }, [trekkNum, buttonDisabled])
+            displayError(errorMessage)
+    }, [trekkNum, buttonDisabled, errorMessage])
     
     return (
         <div style={{
--- src/pages/lotteri.page.js
+++ src/pages/lotteri.page.js
@@ -9,6 +9,7 @@ const Lotteri = () => {
     const [chosenParticipants, setChosenParticipants] = useState([])
     const [winner, setWinner] = useState('...')
     const [drawState, setDrawState] = useState(' ')
+    const [errorMessage, setErrorMessage] = useState('')
 
     const catchParticipants = (participantArray) => {
         // console.log({participantArray})
@@ -28,7 +29,8 @@ const Lotteri = () => {
                         <td className="container">
                             <div className="results" style={{fontSize: '20px', marginTop: '-150px'}} >{drawState}</div>
                             <div className={'results vinner'}>{winner}</div>
-                            <Button amountOfParticipants={chosenParticipants.length} numHandler={catchNumber} setDrawState={setDrawState} />
+                            <Button amountOfParticipants={chosenParticipants.length} numHandler={catchNumber} setDrawState={setDrawState} displayError={setErrorMessage} />
+                            <div className='errorMessage'>{errorMessage}</div>
                         </td>
                         <td className="sideBar">
                             <ParticipantList participantHandler={catchParticipants} />
