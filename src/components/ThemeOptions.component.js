6102dca6ff3eedc290b28df8042f89efa1d97d62
Andrrs64 <andersthorsrud@hotmail.no>
2022-11-17 12:12:38 +0100

    added theme drop down menu

--- src/components/ThemeOptions.component.js
+++ src/components/ThemeOptions.component.js
@@ -1,42 +1,83 @@
-import { keyboard } from "@testing-library/user-event/dist/keyboard"
 import { useEffect, useState } from "react"
 
 const ThemeSelect = () => {
     const themes = {
         Light: {
-            header: '#00A499',
+            Name: 'lyst',
+            header: 'rgb(0, 164, 153)',
             background: 'rgb(255,255,255)',
-            sideBar: '#eeeeee'
+            sideBar: 'rgb(238, 238, 238)',
+            textColor: 'rgb(0, 0, 0)'
         },
         Dark: {
-            header: '#00A499',
-            background: 'rgb(255,255,255)',
-            sideBar: '#eeeeee'
+            Name: 'Mørkt',
+            header: 'rgb(0, 164, 153)',
+            background: 'rgb(77, 77, 77)',
+            sideBar: 'rgb(56, 56, 56)',
+            textColor: 'rgb(255, 255, 255)'
+        },
+        Black: {
+            Name: 'Svart',
+            header: 'rgb(0, 0, 0)',
+            background: 'rgb(77, 77, 77)',
+            sideBar: 'rgb(56, 56, 56)',
+            textColor: 'rgb(255, 255, 255)'
         }
     }
+    const dropDownStates = {
+        colorTheme: 'hidden'
+    }
 
-    const [currentThemeKey, setCurrentThemeKey] = useState('Light')
+    const [currentThemeKey, setCurrentThemeKey] = useState('Dark')
+    const [lastThemeKey, setLastThemeKey] = useState()
+    const [themeDropDownActive, setThemeDropDownActive] = useState(false)
 
     useEffect(() => {
-        document.getElementById(`${currentThemeKey}-theme`).disabled = true
-    }, [currentThemeKey])
+        changeTheme()
+    }, [currentThemeKey, themeDropDownActive])
 
-    const changeTheme = (themeKey) => {
-        document.getElementById(`${currentThemeKey}-theme`).disabled = false
-        setCurrentThemeKey(themeKey)
+    const changeTheme = () => {
+        if(lastThemeKey) {
+            document.getElementById(`${lastThemeKey}-theme`).disabled = false
+        }
+        document.getElementById(`${currentThemeKey}-theme`).disabled = true
+        setLastThemeKey(currentThemeKey)
+
+        const root = document.querySelector(':root')
+        const theme = themes[currentThemeKey]
+
+        root.style.setProperty('--headerColor', theme.header)
+        root.style.setProperty('--backgroundColor', theme.background)
+        root.style.setProperty('--sidebarColor', theme.sideBar)
+        root.style.setProperty('--textColor', theme.textColor)
+    }
+
+    const themeDropDown = () => {
+        if(themeDropDownActive == false) {
+            setThemeDropDownActive(true)
+            return
+        }
+        setThemeDropDownActive(false)
     }
 
     return (
         <div className="themeOptions">
+            <hr />
+            <button className="dropDownButton" onClick={themeDropDown}>Tema</button>
             <div className="colorThemeMenu">
                 {Object.keys(themes).map((themeKey, i) => {
                     return(
-                        <button className={'colorThemeButton'} id={`${themeKey}-theme`} key={themeKey} onClick={() => changeTheme(themeKey)}>
-                            {themeKey}
+                        <button className={`colorThemeButton ${function(){
+                            if(themeDropDownActive == false) {
+                                return 'hidden'
+                            }
+                        }()}`} id={`${themeKey}-theme`} key={themeKey} onClick={() => {setCurrentThemeKey(themeKey)}}>
+                            {themes[themeKey].Name}
                         </button>
                     )
                 })}
             </div>
+            <hr />
         </div>
     )
 }
