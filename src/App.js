3230e42d48cb0f75926a67dcb7b7568e6dcbd3fc
Andrrs64 <andersthorsrud@hotmail.no>
2022-11-18 08:19:42 +0100

    Cleaned up App js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import './App.css';
-import Button from './components/Button.component.js';
-import Footer from './components/Footer.component';
 import Lotteri from './pages/Lotteri.page';
 
 function App() {
   return (
-    // <div className="App">
-    //   <div className='Header'>
-    //     <a href="https://www.rubic.no/"><img src={logo} className="logo" /> </a>
-    //     <h2 className='topTekst'> Vinlotteri for Rubic AS </h2>
-    //   </div>
-     
-    //   <Button />
-    //   <Footer />
-    // </div>
-
     <Lotteri />
   );
 }
